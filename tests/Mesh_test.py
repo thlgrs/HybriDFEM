f7e2524f47c500890991f3cda91f2b4c6c3a68e7
thlgrs <thlegros01@gmail.com>
2025-06-09 19:36:17 +0200

    vs code config

--- tests/Mesh_test.py
+++ tests/Mesh_test.py
@@ -1,5 +1,6 @@
 from Objects.FE_2D import Mesh
 
+
 def usage():
     points = [(0, 0), (1, 0), (1, 1), (0, 1)]
     with Mesh(points, "triangle", 0.1, name="test_mesh") as mesh:
@@ -15,7 +16,12 @@ def test_methods():
     name = "test_mesh"
 
     # Initialize the Mesh class
-    mesh = Mesh(points=square_points, element_type=element_type, element_size=element_size, name=name)
+    mesh = Mesh(
+        points=square_points,
+        element_type=element_type,
+        element_size=element_size,
+        name=name,
+    )
 
     # Generate the mesh
     print("Generating mesh...")
@@ -23,22 +29,22 @@ def test_methods():
 
     # Plot the generated mesh
     print("Plotting mesh...")
-    mesh.plot(save_path='mesh_0.png')
+    mesh.plot(save_path="mesh_0.png")
 
     # Add a point to the mesh and regenerate
     print("Adding a point and regenerating the mesh...")
     mesh.add_point(0.5, 0.5, regen=True)
-    mesh.plot(save_path='mesh_1.png')
+    mesh.plot(save_path="mesh_1.png")
 
     # Change the element size and update the mesh
     print("Changing element size to 0.05 and updating the mesh...")
     mesh.change_size(new_size=0.05, regen=True)
-    mesh.plot(save_path='mesh_2.png')
+    mesh.plot(save_path="mesh_2.png")
 
     # Change the element type to "quad" and update the mesh
     print("Changing element type to 'quad' and updating the mesh...")
     mesh.change_type(new_type="quad", regen=True)
-    mesh.plot(save_path='mesh_3.png')
+    mesh.plot(save_path="mesh_3.png")
 
     # Find specific points in the mesh
     print("Finding points near (0.5, 0.5)...")
@@ -50,17 +56,33 @@ def cantilever(height, length, sort, size=0.05, plot=False):
     points = [(0, 0), (length, 0), (length, height), (0, height)]
     mesh = Mesh(points, sort, size, name="output/beam_{}{}".format(sort, size))
     mesh.generate_mesh()
-    if plot : mesh.plot()
+    if plot:
+        mesh.plot()
+
 
 def gen_sizes(sizes=None):
     if sizes is None:
-        sizes = [1, 3/4, 1/2, 1/3, 1/4, 1/6, 1/8, 1/12, 1/16, 1/24, 1/32, 1/48, 1/64, 1/128]
+        sizes = [
+            1,
+            3 / 4,
+            1 / 2,
+            1 / 3,
+            1 / 4,
+            1 / 6,
+            1 / 8,
+            1 / 12,
+            1 / 16,
+            1 / 24,
+            1 / 32,
+            1 / 48,
+            1 / 64,
+            1 / 128,
+        ]
     for size in sizes:
-        cantilever(1, 10, sort="tri",  size=size)
+        cantilever(1, 10, sort="tri", size=size)
         cantilever(1, 10, sort="quad", size=size)
 
 
 if __name__ == "__main__":
     test_methods()
-    #gen_sizes()
-
+    # gen_sizes()
